33d1908ab56788cb7db6490ed7af51d4ea8fce2d
narumichi0710 <narumichi7373@gmail.com>
2020-10-17 17:34:10 +0900

    バージョン番号を追加しました

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Shaire Chat') }}</title>
+    <title>{{ config('app.name', 'Shaire Chat v1.0') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
